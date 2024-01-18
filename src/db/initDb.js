aa9bc3e3213fa6601524d920d338c1c31633e4f7
Aaron23HAB <drummer.eiron@gmail.com>
2024-01-18 23:31:22 +0100

    ADD||tablas middlewares etc

---
+++ src/db/initDb.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const { getConnection } = require('./db');
+
+async function main() {
+  let connection;
+
+  try {
+    connection = await getConnection();
+    console.log('Creando tablas');
+
+    await connection.query(`
+            CREATE TABLE users (
+                id INT PRIMARY KEY AUTO:INCREMENT,
+                email VARCHAR(100) UNIQUE NOT NULL,
+                password VARCHAR(100) NOT NULL
+            );
+        `);
+
+    await connection.query(`
+        CREATE TABLE notes (
+            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            user_id INT NOT NULL,
+            texto TEXT,
+            categorie_id INT,
+            image VARCHAR(100),
+            eliminated BOOLEAN DEFAULT FALSE,
+            FOREING KEY (user_id) REFERENCES users(id),
+            FOREING KEY (categorie_id) REFERENCES categories(id)
+        );
+    `);
+
+    await connection.query(`
+          CREATE TABLE categories (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            nombre VARCHAR(50) NOT NULL
+          )
+    `)
+  } catch (error) {
+    console.error(error);
+  } finally {
+    if (connection) connection.release();
+    process.exit();
+  }
+}
+
+main();
